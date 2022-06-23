1a4bff8d3d193c11916f2ecf20d151eb6c41330b
WindSoilder <WindSoilder@outlook.com>
2022-06-23 19:59:10 +0800

    add subscribe api

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,12 @@
+use serde::{Deserialize, Serialize};
 use std::net::TcpListener;
+use surf::http::{Body, Method, Url};
+
+#[derive(Serialize, Clone)]
+struct Subscription {
+    name: String,
+    email: String,
+}
 
 #[async_std::test]
 async fn health_check_works() {
@@ -9,6 +17,86 @@ async fn health_check_works() {
         .await
         .expect("Failed to execute request");
     assert!(resp.status().is_success());
+}
+
+#[async_std::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+
+    let url =
+        Url::parse(&format!("{app_address}/subscriptions")).expect("failed to parse url address");
+
+    let mut request = surf::Request::builder(Method::Post, url).build();
+    request
+        .body_form(&Subscription {
+            name: "le guin".to_string(),
+            email: "ursula_le_guin@gmail.com".to_string(),
+        })
+        .unwrap();
+
+    let client = surf::client();
+    let response = client
+        .send(request)
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status(), 200);
+}
+
+#[async_std::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    #[derive(Clone, Serialize, Deserialize)]
+    struct Subscription {
+        name: Option<String>,
+        email: Option<String>,
+    }
+    // Arrange
+    let app_address = spawn_app();
+    let test_cases = vec![
+        (
+            Subscription {
+                name: Some("le guin".to_string()),
+                email: None,
+            },
+            "missing the email",
+        ),
+        (
+            Subscription {
+                name: None,
+                email: Some("ursula_le_guin@gmail.com".to_string()),
+            },
+            "missing the name",
+        ),
+        (
+            Subscription {
+                name: None,
+                email: None,
+            },
+            "missing both name and email",
+        ),
+    ];
+    let url =
+        Url::parse(&format!("{app_address}/subscriptions")).expect("failed to parse url address");
+    let client = surf::client();
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let mut request = surf::Request::builder(Method::Post, url.clone()).build();
+        request.body_form(&invalid_body).unwrap();
+        let response = client
+            .send(request)
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            response.status(),
+            400,
+            // Additional customized error message on test failure
+            "The API did not fail with 400 Bad Request when payload was {}.",
+            error_message
+        );
+    }
 }
 
 // Launch our application in the background ~somehow~
