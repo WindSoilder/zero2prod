ced54800d6b1f9fd73f0ca40f76953981d8f60cd
WindSoilder <WindSoilder@outlook.com>
2022-07-20 06:34:27 +0800

    introduce the idea of sink, and let get_subscriber make use of it, so our tests will not output too much logs by default

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let server = get_server(
