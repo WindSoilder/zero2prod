c7b725c3357b0f7333679ab6c65b2418a4d608b4
WindSoilder <WindSoilder@outlook.com>
2022-07-20 18:15:06 +0800

    fmt code

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -156,9 +156,10 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create databse.
-    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
