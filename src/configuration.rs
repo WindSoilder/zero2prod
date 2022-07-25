86b5a0d13ff714a8497ef32e1fb73c6fb9cbba7d
WindSoilder <WindSoilder@outlook.com>
2022-07-25 17:12:04 +0800

    refactor: in database setting, use with_db, without_db to return pgConnectOptions rather than string

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,7 @@
 use secrecy::ExposeSecret;
 use secrecy::Secret;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
 
 #[derive(serde::Deserialize, Debug)]
 pub struct Settings {
@@ -9,6 +11,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize, Debug)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -40,7 +43,11 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(&environment_filename),
         ))
-        .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
 
     // our Setting type
@@ -48,25 +55,16 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::get_server;
@@ -9,9 +8,7 @@ async fn main() -> tide::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let server = get_server(
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret()).unwrap(),
-    );
+    let server = get_server(PgPool::connect_lazy_with(configuration.database.with_db()));
 
     server
         .listen(format!(
