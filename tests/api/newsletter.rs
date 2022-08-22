a8cefa4b696a9b18710178d608a5803d7b4e0524
WindSoilder <WindSoilder@outlook.com>
2022-08-22 14:46:03 +0800

    9.3: add happy path test

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,4 @@
-use crate::helpers::{spawn_app, Subscription, TestApp};
+use crate::helpers::{spawn_app, ConfirmationLinks, Subscription, TestApp};
 use surf::{StatusCode, Url};
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
@@ -35,8 +35,38 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     assert_eq!(response.status(), StatusCode::Ok);
 }
 
+#[async_std::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    // Arrange
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "html": "<p>Newsletter body as HTML</p>"
+    });
+    let url =
+        Url::parse(&format!("{}/newsletters", app.address)).expect("failed to parse url address");
+    let response = surf::post(url)
+        .body_json(&newsletter_request_body)
+        .expect("Failed to set body json")
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::Ok);
+}
+
 /// Use the public API of the application under test to create an unconfirmed subscriber.
-async fn create_unconfirmed_subscriber(app: &TestApp) {
+async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = Subscription {
         name: Some("le guin".to_string()),
         email: Some("ursula_le_guin@gmail.com".to_string()),
@@ -49,6 +79,23 @@ async fn create_unconfirmed_subscriber(app: &TestApp) {
         .mount_as_scoped(&app.email_server)
         .await;
     let resp = app.post_subscriptions(&body).await;
+    if resp.status().is_client_error() || resp.status().is_server_error() {
+        panic!("post subscripitons during create_unconfirmed_subscriber shouldn't failed");
+    }
+
+    let email_request = app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+    app.get_confirmation_links(&email_request)
+}
+
+async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_link = create_unconfirmed_subscriber(app).await;
+    let resp = surf::get(confirmation_link.html).await.unwrap();
     if resp.status().is_client_error() || resp.status().is_server_error() {
         panic!("post subscripitons during create_unconfirmed_subscriber shouldn't failed");
     }
