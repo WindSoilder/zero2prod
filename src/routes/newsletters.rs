919b064dce7746d2cc170027a6cc1d50e0b54e3a
WindSoilder <WindSoilder@outlook.com>
2022-08-22 16:25:48 +0800

    9.8.3: Remove Some Boilerplate

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -68,16 +68,7 @@ async fn get_confirmed_subscribers(
     pool: &PgPool,
 ) -> std::result::Result<Vec<std::result::Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error>
 {
-    // We only need `Row` to map the data coming out of this query.
-    // Nesting its definition inside the function itself is a simple way
-    // to clearly communicate this coupling (and to ensure it doesn't
-    // get used eelsewhere by mistake.)
-    struct Row {
-        email: String,
-    }
-
-    let rows = sqlx::query_as!(
-        Row,
+    let confirmed_subscribers = sqlx::query!(
         r#"
         SELECT email
         FROM subscriptions
@@ -85,15 +76,13 @@ async fn get_confirmed_subscribers(
         "#
     )
     .fetch_all(pool)
-    .await?;
-    // Map into the domain type
-    let confirmed_subscribers = rows
-        .into_iter()
-        .map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Ok(ConfirmedSubscriber { email }),
-            Err(error) => Err(anyhow::anyhow!(error)),
-        })
-        .collect();
+    .await?
+    .into_iter()
+    .map(|r| match SubscriberEmail::parse(r.email) {
+        Ok(email) => Ok(ConfirmedSubscriber { email }),
+        Err(error) => Err(anyhow::anyhow!(error)),
+    })
+    .collect();
     Ok(confirmed_subscribers)
 }
 
